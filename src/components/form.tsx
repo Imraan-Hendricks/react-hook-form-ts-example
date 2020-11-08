ffdd4b6c4d03d243dee528bd6ec216e2bfd758db
Imraan-Hendricks <imraan.hndx@gmail.com>
2020-11-08 13:00:42 +0200

    added rhf register and handleSubmit

--- src/components/form.tsx
+++ src/components/form.tsx
@@ -1,23 +1,51 @@
 import React from 'react';
+import { useForm } from 'react-hook-form';
+
+interface Profile {
+  firstName: string;
+  lastName: string;
+  age: number;
+}
 
 export const Form: React.FC = () => {
+  const { register, handleSubmit } = useForm<Profile>();
+
+  const onSubmit = handleSubmit((data) => {
+    alert(JSON.stringify(data));
+  });
+
   return (
     <div>
       <h1>Profile</h1>
-      <form>
+      <form onSubmit={onSubmit}>
         <div>
           <label>First Name:</label>
-          <input name='firstName' placeholder='enter first name' type='text' />
+          <input
+            ref={register()}
+            name='firstName'
+            placeholder='enter first name'
+            type='text'
+          />
         </div>
 
         <div>
           <label>Last Name:</label>
-          <input name='lastName' placeholder='enter last name' type='text' />
+          <input
+            ref={register()}
+            name='lastName'
+            placeholder='enter last name'
+            type='text'
+          />
         </div>
 
         <div>
           <label>Age:</label>
-          <input name='age' placeholder='enter age' type='text' />
+          <input
+            ref={register()}
+            name='age'
+            placeholder='enter age'
+            type='text'
+          />
         </div>
 
         <button type='submit'>Save</button>
