2dc0310d3d2a8a820ea17234c97e9aa27d5397b5
Imraan-Hendricks <imraan.hndx@gmail.com>
2020-11-09 13:33:50 +0200

    added rhf errors

--- src/components/form.tsx
+++ src/components/form.tsx
@@ -8,7 +8,7 @@ interface Profile {
 }
 
 export const Form: React.FC = () => {
-  const { register, handleSubmit } = useForm<Profile>();
+  const { register, handleSubmit, errors } = useForm<Profile>();
 
   const onSubmit = handleSubmit((data) => {
     alert(JSON.stringify(data));
@@ -21,31 +21,53 @@ export const Form: React.FC = () => {
         <div>
           <label>First Name:</label>
           <input
-            ref={register()}
+            ref={register({
+              required: 'first name required',
+              minLength: { value: 3, message: 'Too short!' },
+              maxLength: { value: 20, message: 'Too long!' },
+              pattern: {
+                value: /^[a-z ,.'-]+$/i,
+                message: 'Invalid first name',
+              },
+            })}
             name='firstName'
             placeholder='enter first name'
             type='text'
           />
+          {errors.firstName && <p>{errors.firstName.message}</p>}
         </div>
 
         <div>
           <label>Last Name:</label>
           <input
-            ref={register()}
+            ref={register({
+              required: 'last name required',
+              minLength: { value: 3, message: 'Too short!' },
+              maxLength: { value: 20, message: 'Too long!' },
+              pattern: {
+                value: /^[a-z ,.'-]+$/i,
+                message: 'Invalid last name',
+              },
+            })}
             name='lastName'
             placeholder='enter last name'
             type='text'
           />
+          {errors.lastName && <p>{errors.lastName.message}</p>}
         </div>
 
         <div>
           <label>Age:</label>
           <input
-            ref={register()}
+            ref={register({
+              required: 'age required',
+              pattern: { value: /^[0-9]*$/, message: 'age must be a number' },
+            })}
             name='age'
             placeholder='enter age'
             type='text'
           />
+          {errors.age && <p>{errors.age.message}</p>}
         </div>
 
         <button type='submit'>Save</button>
